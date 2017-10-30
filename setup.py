87f7d0677503fe8541265c5442559d40edb832b1
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-10-30 21:54:46 -0200

    Base extractor with regex

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="molo",
+    version="0.1.0",
+    author="VulcanoAhab",
+    packages=["molo"],
+    url="https://github.com/VulcanoAhab/molo.git",
+    description="Processing text",
+)
